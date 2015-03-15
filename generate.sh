ff83287601c8943184306984f5e62c0e3dd5be7c
frantisekfarka <frantisek@farka.eu>
2015-03-15 22:20:19 +0000

    arial + formating

--- generate.sh
+++ generate.sh
@@ -2,6 +2,12 @@
 
 SED=sed
 INTER=labels.tex
+TEMPLATE=template
+TEX=pdflatex
+EXT=pdf
+MV=mv
+CAT=cat
+CUT="cut -d;"
 
 if [ $# -eq 0 ]; then
 	echo "Provide a filename to process ..."
@@ -10,9 +16,10 @@ fi
 
 for FILE in $@ 
 do
-	OUT=`basename $FILE .csv`".tex"
 	echo Processing file $FILE
-	$SED 's/;/}{/g;s/^/\\mylabel{/;s/$/}/' $FILE  > $INTER
-
-
+	$CAT $FILE | $CUT -f1,2,3,4,6,8,9,10,11,12 \
+		| $SED 's/;/}{/g;s/^/\\mylabel{/;s/$/}/' > $INTER 
+	$TEX $TEMPLATE
+	OUT=`basename $FILE .csv`".$EXT"
+	$MV ${TEMPLATE}.$EXT $OUT
 done

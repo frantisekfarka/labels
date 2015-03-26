1ab9e00def3870ff544859af76663e6bcc081fbf
frantisekfarka <frantisek@farka.eu>
2015-03-26 21:42:31 +0000

    ethanol label version

--- generate.sh
+++ generate.sh
@@ -13,14 +13,30 @@ DEST_PATH=out
 
 . .config
 
-if [ $# -eq 0 ]; then
-	echo "Provide a filename to process ..."
+if [ $# -le 1 ]; then
+	echo "Provide a filename(s) to process ..."
 	exit 1
 fi
+
+if [ $1 = template ]; then
+	echo "Using normal template"
+	TEMPLATE=template
+else 
+	if [ $1 = ethanol ]; then
+		TEMPLATE=ethanol
+		echo "Using Etanol template"
+	else
+		echo "Unknow template $1. Die!"
+		exit 1
+	fi
+fi
+
+shift
 
 for FILE in $@ 
 do
 	echo Processing file $FILE
+		#| head -n 20 \
 	$CAT $FILE | $CUT -f1,2,3,4,6,8,9,10,11,12 \
 		| $SED 's/\&/\\&/g;s/;/}{/g;s/^/\\mylabel{/;s/$/}/' > $INTER 
 	$TEX $TEMPLATE
